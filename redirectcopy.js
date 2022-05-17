6324a11e7a100edce41b01701d48ee572915613f
ThanhThuanReal <105635832+ThanhThuanReal@users.noreply.github.com>
2022-05-17 10:19:59 +0700

    Add files via upload

---
+++ redirectcopy.js
@@ -0,0 +1,17 @@
+function FSAddOriginalLink(){
+    var body_element = document.getElementsByTagName('body')[0];
+    var selection;
+    selection = window.getSelection();
+    var pagelink = "<br/><br/> LIÊN KẾT CỘNG TÁC MÔI GIỚI 0935 881 393 : <a href='https://nhaphoroyalhouse.blogspot.com/'>https://nhaphoroyalhouse.blogspot.com/</a>";
+    var copytext = pagelink;
+    var newdiv = document.createElement('div');
+    newdiv.style.position='absolute';
+    newdiv.style.left='-99999px';
+    body_element.appendChild(newdiv);
+    newdiv.innerHTML = copytext;
+    selection.selectAllChildren(newdiv);
+    window.setTimeout(function() {
+        body_element.removeChild(newdiv);
+    },0);
+}
+document.oncopy = FSAddOriginalLink;
